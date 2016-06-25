55f319ea3d11395458f7a90a0cfa6af03572bdcc
Justin Flintoff <JustinFlintoff@gmail.com>
2016-06-25 16:17:20 -0700

    Changed alot of layout junk, also added some social links.

---
+++ jquery.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+	var bodyHeight = $('.iAmTheBody').height();
+	var bodyWidth = $('.iAmTheBody').width();
+	console.log ("bodyWidth");
+	console.log ("bodyHeight");
+});
